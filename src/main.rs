c49de8b6137b9974706cc6a1a072f70e4a4dba00
Youka <spanknebel.borken@t-online.de>
2020-04-09 22:21:58 +0200

    added compression middleware

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ mod services;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Connect to database
-    let data = actix_web::web::Data::new(std::sync::Mutex::new(
-        rusqlite::Connection::open(config::DB_PATH).expect(&format!("Open SQLite in file '{}' failed?!", config::DB_PATH))
-    ));
+    let conn = rusqlite::Connection::open(config::DB_PATH).expect(&format!("Open SQLite file '{}' failed?!", config::DB_PATH));
+    // Pack application shared data
+    let data = actix_web::web::Data::new(std::sync::Mutex::new(conn));
     // Start http server
     actix_web::HttpServer::new(move ||
         actix_web::App::new()
             .app_data(data.clone())
             .configure(config::register_services)
+            .wrap(actix_web::middleware::Compress::default())
     )
     .bind("127.0.0.1:".to_string() + config::PORT)?
     .run().await
