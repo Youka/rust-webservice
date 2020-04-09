f987992d079f3008a5f19de1957a4f645064944d
Youka <spanknebel.borken@t-online.de>
2020-04-09 23:01:08 +0200

    added server logging

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod services;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // Initialize logging
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
     // Connect to database
     let conn = rusqlite::Connection::open(config::DB_PATH).expect(&format!("Open SQLite file '{}' failed?!", config::DB_PATH));
     // Pack application shared data
@@ -12,6 +15,7 @@ async fn main() -> std::io::Result<()> {
         actix_web::App::new()
             .app_data(data.clone())
             .configure(config::register_services)
+            .wrap(actix_web::middleware::Logger::default())
             .wrap(actix_web::middleware::Compress::default())
     )
     .bind("127.0.0.1:".to_string() + config::PORT)?
