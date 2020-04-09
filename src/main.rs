e6ff046956f556046a3ab840314ada1b9ca59fe7
Youka <spanknebel.borken@t-online.de>
2020-04-09 21:57:43 +0200

    moved database connection into application

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod services;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Connect to database
-    let conn = rusqlite::Connection::open(config::DB_PATH).expect(&format!("Open SQLite in file '{}' failed?!", config::DB_PATH));
-    conn.close().expect("Closing SQLite connection failed?!");
+    let data = actix_web::web::Data::new(std::sync::Mutex::new(
+        rusqlite::Connection::open(config::DB_PATH).expect(&format!("Open SQLite in file '{}' failed?!", config::DB_PATH))
+    ));
     // Start http server
-    actix_web::HttpServer::new(||
+    actix_web::HttpServer::new(move ||
         actix_web::App::new()
+            .app_data(data.clone())
             .configure(config::register_services)
     )
     .bind("127.0.0.1:".to_string() + config::PORT)?
